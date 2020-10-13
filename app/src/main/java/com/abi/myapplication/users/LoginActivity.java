9047a75fb2136bfed8226b0b7a305a4bc41814bc
Wahyuabi <wahyu.18415016@student.ubl.ac.id>
2020-10-13 12:59:14 +0700

    Penjualan Buku

---
+++ app/src/main/java/com/abi/myapplication/users/LoginActivity.java
@@ -0,0 +1,33 @@
+package com.abi.myapplication.users;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.abi.myapplication.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button btnRegis;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btnRegis = (Button) findViewById(R.id.btnRegis);
+
+        btnRegis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(LoginActivity.this, RegisActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+    }
+}
